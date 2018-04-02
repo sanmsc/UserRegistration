73ac8debd368bd66058d0fc7114c95448404a65e
sandmsc <sunmscc@gmail.com>
2018-04-02 10:11:49 -0400

    Add project files.

---
+++ UserRegistration/Controllers/RedirectController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UserRegistration.Controllers
+{
+    public class RedirectController : Controller
+    {
+        // GET: Redirect
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Get All EMPs
+        public ActionResult GetAll()
+        {
+            return View();
+        }
+
+
+
+    }
+}
