4bf5c706c9a0fd04c17547108de926a787da7522
sanmsc <sunmscc@gmail.com>
2018-04-03 12:46:38 -0400

    Adding EMP coontroller
    Adding Emp controller

---
+++ UserRegistration/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserRegistration.Models;
+
+namespace UserRegistration.Controllers
+{
+    public class EmployeeController:Controller
+    {
+
+        public ActionResult List()
+        {
+            var repo = new EmployeeRepository();
+            var employee = repo.GetAll();
+
+            return View(employee);
+
+
+        }
+    }
+}
---
+++ UserRegistration/Models/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UserRegistration.Models
+{
+    public class Employee
+            {
+
+        public int EmployeeId { get; set; }
+
+        public String Name { get; set; }
+
+
+        public String Mail { get; set; }
+
+
+        public String Dept { get; set; }
+
+
+        public DateTime StartDate { get; set; }
+        
+    }
+
+    class EmployeeRepository
+    {
+        private static List<Employee> employee = new List<Employee>()
+        {
+        new Employee{EmployeeId=1 , Name= "A",  Mail="a@example.com" , Dept="ITD" , StartDate=DateTime.Today   },
+        new Employee{EmployeeId=2 , Name= "B",  Mail="b@example.com" , Dept="IS" , StartDate=DateTime.Today   },
+        new Employee{EmployeeId=3 , Name= "C",  Mail="c@example.com" , Dept="IST" , StartDate=DateTime.Today   }
+        };
+       public List<Employee> GetAll()
+        {
+
+            return employee;
+        }
+    }
+
+}
