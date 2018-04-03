045c2e7877ff95e7ef274c1280f819616b39ec2d
sanmsc <sunmscc@gmail.com>
2018-04-03 14:48:38 -0400

    Adding Teammember Modoel
    teammemebr models

---
+++ UserRegistration/Models/TeamMember.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UserRegistration.Models
+{
+    public class TeamMember
+    {
+
+
+        public int Id { get; set; }
+
+        public String Name { get; set; }
+
+
+        public String Token { get; set; }
+
+
+        public String PageUrl { get; set; }
+
+               
+
+    }
+}
